104a709732da4c85269704882c83122bf2eeb7b0
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-10 14:03:53 +0300

    Added register functionality

---
+++ scripts/userServices.js
@@ -0,0 +1,90 @@
+import { navigate } from "./router.js";
+
+const endpoints = {
+    register: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAmv0aYzWuLU-FGBowxuSLFxUDycT053Rc`
+}
+
+const request = {
+    
+    post: async (url, body) => await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(body),
+            returnSecureToken: true
+        })
+}
+
+
+const validateCredentials = {
+
+    register: (email, password, repPassword) => {
+
+        if(!checkForTruthyInputs(email, password, repPassword)) return false;
+
+        if(!checkForSpecificLength(password, 6)) return false;
+
+        if(password !== repPassword) return false;
+
+        return true;
+    },
+
+    login: (email, password) => {
+               
+        if(!checkForTruthyInputs(email, password)) return false;
+        
+        if(!checkForSpecificLength(password, 6)) return false;
+
+        return true;
+    }
+}
+
+function getInputDataFromForm(formElement) {
+
+    let email = formElement.querySelector('#email').value;
+    let password = formElement.querySelector('#password').value;
+    let repPassword = formElement.querySelector('#password') ? formElement.querySelector('#password').value : null;
+
+    return {
+        email,
+        password,
+        repPassword
+    }
+}
+
+function checkForSpecificLength(item, wantedLength) {
+
+    if(typeof item == "object" || typeof item == "array") return false;
+
+    if(item.length >= wantedLength) return true;
+
+    return false;
+
+}
+
+function checkForTruthyInputs(...items) {
+
+    let result = true;
+
+    items.forEach(item => {
+        
+        if(!item) result = false;
+    })
+
+    return result;
+}
+
+export async function register() {
+
+    let registerForm = document.querySelector('.registerForm');
+
+    let { email, password, repPassword } = getInputDataFromForm(registerForm);
+
+    // TODO: Show notif
+
+    if(!validateCredentials.register(email, password, repPassword)) return;
+
+    let response = await request.post(endpoints.register, {email, password});
+
+    if(!response.ok) return;
+
+    navigate('/login');
+}
--- templates/register.js
+++ templates/register.js
@@ -1,4 +1,5 @@
 import { html } from 'https://unpkg.com/lit-html?module';
+import { register } from '../scripts/userServices.js';
 import headerTemplate from './partials/header.js';
 import footerTemplate from './partials/footer.js';
 
@@ -9,7 +10,7 @@ ${headerTemplate()}
                     <h1>Sign up</h1>
                 </div>
                 <div class="register-page__body">
-                   <div class="submit-form">
+                   <div class="submit-form registerForm">
                        <label for="email">Email:</label>
                        <input type="email" id="email" class="register-email"/>
 
@@ -17,8 +18,8 @@ ${headerTemplate()}
                        <input type="password" id="password" class="register-password"/>
 
                        <label for="repPassword">Repeat Password:</label>
-                       <input type="repPassword" id="repPassword" class="register-repPassword"/>
-                       <a href="/login" class="btn btn--primary">Sign up</a>
+                       <input type="password" id="repPassword" class="register-repPassword"/>
+                       <a href="/login" class="btn btn--primary" @click=${register}>Sign up</a>
                    </div>
                 </div>
                 <div class="register-page__footer">
