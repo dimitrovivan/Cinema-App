cd8d4066763ab5353d294774cbcaf914b79ee1d4
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-12 10:34:50 +0300

    Added pay functionality and reserve seats

--- scripts/movieServices.js
+++ scripts/movieServices.js
@@ -1,3 +1,6 @@
+import { showNotification } from './notifications.js';
+import { navigate } from './router.js';
+import { request } from './util.js';
 
 export function listenForSeatClickAndChangeState(e) {
 
@@ -11,3 +14,67 @@ export function listenForSeatClickAndChangeState(e) {
         target.classList.add('selected');
     }
 }
+
+export function showAndSubmitConfirmForm() {
+
+    let selectedSeats = getSelectedSeats();
+
+    if(selectedSeats.length < 1) return showNotification.error("Please select a seat");
+    
+    let confirmSection = document.querySelector('.confirm-tickets');
+
+    confirmSection.style.display = "block";
+
+}
+
+export async function payForTickets() {
+
+    let selectedSeats = getSelectedSeats();
+
+    let firstNameElement = document.querySelector('#first-name');
+    let lastNameElement = document.querySelector('#last-name');
+    let cardIdElement = document.querySelector('#card-id');
+
+    if(!firstNameElement.value || !lastNameElement.value || !cardIdElement.value) return showNotification.error("You must fill all inputs");
+    
+    let [pathName, movieId, streamHour] = location.pathname.split('/').filter(i => i != "");
+
+    let movieURL = `https://cinema-app-7733d-default-rtdb.firebaseio.com/movies/${movieId}.json`;
+
+    let getResponse = await  request.get(movieURL);
+
+    let data = await getResponse.json();
+
+    let allStreams = data.streams;
+
+    let chosenStream = allStreams[streamHour];
+
+    Array.from(selectedSeats).map(seat => chosenStream[seat.dataset.seat] = "reserved");
+
+    let changedStreams = {};
+     
+    Object.keys(allStreams).forEach( key => {
+
+       if (key == streamHour) {
+           changedStreams[key] = chosenStream;
+       } else {
+           changedStreams[key] = allStreams[key];
+       }
+    } );
+
+    let patchBody = {  streams: changedStreams }
+
+    let patchResponse = await request.patch(movieURL, patchBody);
+
+    if(!patchResponse.ok) return showNotification.error("Something went wrong... Please try again");
+
+    navigate('/');
+
+    return showNotification.success(`Successfully bought ${selectedSeats.length} ${selectedSeats.length > 1 ? "tickets" : "ticket"}!`)
+
+}
+
+const getSelectedSeats = () => Array.from(document.querySelectorAll('.cinema-hall__list-item'))
+                                    .reduce( (acc, seat) => seat.classList.contains('selected') ? [...acc, seat] : [...acc], [])
+
+
--- templates/cinema-hall.js
+++ templates/cinema-hall.js
@@ -1,5 +1,6 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 import headerTemplate from './partials/header.js';
+import {listenForSeatClickAndChangeState, showAndSubmitConfirmForm, payForTickets} from '../scripts/movieServices.js';
 
 export default (
     {
@@ -27,7 +28,7 @@ ${headerTemplate(isLogged)}
     </div>
     
     <div class="confirm-tickets__footer">
-    <button class="btn btn--primary">Pay</button>
+    <button class="btn btn--primary" @click=${payForTickets}>Pay</button>
     </div>
   </div>
     <div class="cinema-hall__header">
@@ -39,37 +40,37 @@ ${headerTemplate(isLogged)}
     </div>
 
     <div class="cinema-hall__body">
-        <ul class="cinema-hall__list">
-            <li class="cinema-hall__list-item ${seats[1]}"></li>
-            <li class="cinema-hall__list-item ${seats[2]}"></li>
-            <li class="cinema-hall__list-item ${seats[3]}"></li>
-            <li class="cinema-hall__list-item ${seats[4]}"></li>
-            <li class="cinema-hall__list-item ${seats[5]}"></li>
-            <li class="cinema-hall__list-item ${seats[6]}"></li>
-            <li class="cinema-hall__list-item ${seats[7]}"></li>
-            <li class="cinema-hall__list-item ${seats[8]}"></li>
-            <li class="cinema-hall__list-item ${seats[9]}"></li>
-            <li class="cinema-hall__list-item ${seats[10]}"></li>
-            <li class="cinema-hall__list-item ${seats[11]}"></li>
-            <li class="cinema-hall__list-item ${seats[12]}"></li>
-            <li class="cinema-hall__list-item ${seats[13]}"></li>
-            <li class="cinema-hall__list-item ${seats[14]}"></li>
-            <li class="cinema-hall__list-item ${seats[15]}"></li>
-            <li class="cinema-hall__list-item ${seats[16]}"></li>
-            <li class="cinema-hall__list-item ${seats[17]}"></li>
-            <li class="cinema-hall__list-item ${seats[18]}"></li>
-            <li class="cinema-hall__list-item ${seats[19]}"></li>
-            <li class="cinema-hall__list-item ${seats[20]}"></li>
-            <li class="cinema-hall__list-item ${seats[21]}"></li>
-            <li class="cinema-hall__list-item ${seats[22]}"></li>
-            <li class="cinema-hall__list-item ${seats[23]}"></li>
-            <li class="cinema-hall__list-item ${seats[24]}"></li>
-            <li class="cinema-hall__list-item ${seats[25]}"></li>
+        <ul class="cinema-hall__list" @click=${listenForSeatClickAndChangeState}>
+            <li class="cinema-hall__list-item ${seats[1]}" data-seat="1"></li>
+            <li class="cinema-hall__list-item ${seats[2]}" data-seat="2"></li>
+            <li class="cinema-hall__list-item ${seats[3]}" data-seat="3"></li>
+            <li class="cinema-hall__list-item ${seats[4]}" data-seat="4"></li>
+            <li class="cinema-hall__list-item ${seats[5]}" data-seat="5"></li>
+            <li class="cinema-hall__list-item ${seats[6]}" data-seat="6"></li>
+            <li class="cinema-hall__list-item ${seats[7]}" data-seat="7"></li>
+            <li class="cinema-hall__list-item ${seats[8]}" data-seat="8"></li>
+            <li class="cinema-hall__list-item ${seats[9]}" data-seat="9"></li>
+            <li class="cinema-hall__list-item ${seats[10]}" data-seat="10"></li>
+            <li class="cinema-hall__list-item ${seats[11]}" data-seat="11"></li>
+            <li class="cinema-hall__list-item ${seats[12]}" data-seat="12"></li>
+            <li class="cinema-hall__list-item ${seats[13]}" data-seat="13"></li>
+            <li class="cinema-hall__list-item ${seats[14]}" data-seat="14"></li>
+            <li class="cinema-hall__list-item ${seats[15]}" data-seat="15"></li>
+            <li class="cinema-hall__list-item ${seats[16]}" data-seat="16"></li>
+            <li class="cinema-hall__list-item ${seats[17]}" data-seat="17"></li>
+            <li class="cinema-hall__list-item ${seats[18]}" data-seat="18"></li>
+            <li class="cinema-hall__list-item ${seats[19]}" data-seat="19"></li>
+            <li class="cinema-hall__list-item ${seats[20]}" data-seat="20"></li>
+            <li class="cinema-hall__list-item ${seats[21]}" data-seat="21"></li>
+            <li class="cinema-hall__list-item ${seats[22]}" data-seat="22"></li>
+            <li class="cinema-hall__list-item ${seats[23]}" data-seat="23"></li>
+            <li class="cinema-hall__list-item ${seats[24]}" data-seat="24"></li>
+            <li class="cinema-hall__list-item ${seats[25]}" data-seat="25"></li>
         </ul>
     </div>
 
     <div class="cinema-hall__footer">
-        <button class="btn btn--primary">Confirm</button>
+        <button class="btn btn--primary" @click=${showAndSubmitConfirmForm}>Confirm</button>
     </div>
 </div>
 
