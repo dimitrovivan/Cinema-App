20f83622d05ad7a6a385b182dad3e0d5710a8988
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-06-08 10:00:15 +0300

    Set up base config on environment and port

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const { restart } = require('nodemon');
-
 const app = express();
-
 const cors = require('./middlewares/cors');
-
+const { PORT } = require('./config/base');
 app.use(cors);
 
 app.get('/test', ( req, res) => {
     res.json({asd: 123});
 })
 
-app.listen(5000, console.log('Server listenning on port 5000'));
+app.listen(PORT, console.log(`Server listenning on port ${PORT}...`));
